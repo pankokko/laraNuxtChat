b73b0149cf3414be79451ec8b891a12b89907f90
pankokko <teshi41@gmail.com>
2020-10-09 09:00:38 +0900

    git commit

--- app/Http/Controllers/Api/AbstractApiController.php
+++ app/Http/Controllers/Api/AbstractApiController.php
@@ -97,6 +97,7 @@ class AbstractApiController extends Controller
      */
     private function convertToUtf8($data)
     {
+
         if (is_string($data)) {
             return mb_detect_encoding($data) === false ? utf8_encode($data) : $data;
         } elseif (is_array($data)) {
